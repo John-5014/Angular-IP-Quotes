9ac9736c3939f216e6333330583fe602b75c470d
John-5014 <kaiserjohn52@gmail.com>
2019-09-22 22:54:32 +0300

    update changes to quotes.components.ts to suitable publisher dates

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -12,10 +12,10 @@ export class QuotesComponent implements OnInit {
   public buttonName:any = 'Show';
 
   quotes = [
-    new Quotes(1, 'David', 'Stephen King', 'True success is impacting the world with the investment within your personality', new Date(2019,0,12)),
-    new Quotes(2, 'Marvin Makhanu', 'Gay Jones', 'Go big or simply go home', new Date(2019,0,16)),
-    new Quotes(3, 'Gabriel Mufasa', 'Abraham Lincoln', 'Our greatest fear is not that we are inadequate but that we are powerfull, our playing small does not serve the world in anyway', new Date(2019,0,5)),
-    new Quotes(4, 'Bilha Waldings', 'Tobitha Marks', 'stay hungry and stay foolish', new Date(2019,0,15))
+    new Quotes(1, 'David', 'Stephen King', 'True success is impacting the world with the investment within your personality', new Date(2019,1,12)),
+    new Quotes(2, 'Marvin Makhanu', 'Gay Jones', 'Go big or simply go home', new Date(2019,6,16)),
+    new Quotes(3, 'Gabriel Mufasa', 'Abraham Lincoln', 'Our greatest fear is not that we are inadequate but that we are powerfull, our playing small does not serve the world in anyway', new Date(2019,9,5)),
+    new Quotes(4, 'Bilha Waldings', 'Tobitha Marks', 'stay hungry and stay foolish', new Date(2019,3,15))
   ]
 
   addNewQuote(quotes) {
