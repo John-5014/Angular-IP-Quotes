2a8b0bd7d29add8bbe974abc4dbcaa08ff72a145
John-5014 <kaiserjohn52@gmail.com>
2019-09-22 22:38:50 +0300

    update stylings and code for error checking

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter  } from '@angular/core';
+import{Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes-form',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes-form.component.css']
 })
 export class QuotesFormComponent implements OnInit {
+  newQuotes = new Quotes(0, "", "", "", new Date());
+  @Output() addQuotes = new EventEmitter<Quotes>();
+  submitQuotes() {
+    this.addQuotes.emit(this.newQuotes);
+    this.newQuotes = new Quotes(0, "", "", "", new Date());
+  }
 
   constructor() { }
 
