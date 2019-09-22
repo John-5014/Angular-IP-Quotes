9ea83bb8dc38dc6436d780db093f3e4a6ac8e0c4
John-5014 <kaiserjohn52@gmail.com>
2019-09-22 21:39:50 +0300

    update changes to components to check for possible errors

--- src/app/quotes-detail/quotes-detail.component.ts
+++ src/app/quotes-detail/quotes-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import { Quotes } from '../quotes';
+
 
 @Component({
   selector: 'app-quotes-detail',
@@ -6,6 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes-detail.component.css']
 })
 export class QuotesDetailComponent implements OnInit {
+  @Input() quotes: Quotes;
+  @Output()isComplete = new EventEmitter <boolean>();
+
+  
+  quoteDelete(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
+  upvote = 0;
+  downvote = 0;
+
+  like() {
+    this.upvote = this.upvote + 1;
+  }
+  unlike() {
+    this.downvote = this.downvote + 1;
+  }
 
   constructor() { }
 
